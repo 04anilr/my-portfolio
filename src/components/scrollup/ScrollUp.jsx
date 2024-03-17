37530fe239da638422dcece91adc1650eb8f226c
anil rajput <120117525+04anilr@users.noreply.github.com>
2024-03-17 20:48:51 +0530

    Update ScrollUp.jsx
    fex;

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -8,10 +8,15 @@ export const ScrollUp = () => {
         if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
         else scrollUp.classList.remove("show-scroll");
     })
-
+    function ScrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }
   return (
-   <a herf="" className="scrollup">
+   <button className="scrollup" onClick={ScrollToTop}>
     <i className="uil uil-arrow-up scrollup_icon"></i>
-   </a>
+   </button>
   )
 }
