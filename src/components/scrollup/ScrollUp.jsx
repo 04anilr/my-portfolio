61efc419cca4eb81bc4fbcd7be0fba97918247dd
anilr <anilr5364@gmail.com>
2024-03-23 02:41:59 +0530

    fix: error

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import "./scrollup.css";
+import './scrollup.css';
 
 export const ScrollUp = () => {
+  const ScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
 
-    window.addEventListener("scroll", function () {
-        const scrollUp = document.querySelector(".scrollup");
-        if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-        else scrollUp.classList.remove("show-scroll");
-    })
+  window.addEventListener("scroll", function () {
+    const scrollUp = document.querySelector(".scrollup");
+    if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
+  });
 
   return (
-   <button className="scrollup" onClick={ScrollToTop}>
-    <i className="uil uil-arrow-up scrollup_icon"></i>
-   </button>
-  )
-}
+    <button className="scrollup" onClick={ScrollToTop}>
+      <i className="uil uil-arrow-up scrollup_icon"></i>
+    </button>
+  );
+};
