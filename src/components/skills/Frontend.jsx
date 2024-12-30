c552962f7f7f9fb4184613eff432ea72f6dd6655
anil rajput <120117525+04anilr@users.noreply.github.com>
2024-12-30 12:23:05 +0530

    Update Frontend.jsx
    feat:

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Frontend = () => {
   return (
     <div className="skills_content">
-    <h3 className="skills_title">Frontend developer</h3>
+    <h3 className="skills_title">Frontend Skills</h3>
     <div className="skills_box">
         <div className="skills_group">
                 <div className="skills_data">
