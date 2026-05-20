b0b43034fe46fee0b097b2ccef5b62f1bdb5240f
04anilr <anilr5364@gmail.com>
2026-05-20 08:36:25 +0530

    feat: fix build error

--- src/components/Home/ScrollDown.jsx
+++ src/components/Home/ScrollDown.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const ScrollDown = () => {
   return (
    <div className='home_scroll'>
-    <a href="" className='home_scroll-button button--flex'>
+    <a href="#about" className='home_scroll-button button--flex'>
     
               <span className="home_scroll-name">Scroll Down</span>
               <i class='uil uil-arrow-down home_scroll-arrow'></i>
--- src/components/Home/Social.jsx
+++ src/components/Home/Social.jsx
@@ -5,20 +5,20 @@ import { FaDev } from "react-icons/fa";
 export const Social = () => {
   return (
     <div className="home_social">
- <a href="https://www.linkedin.com/in/anil-rajput-6411a4233/" className='home_social-icon' target='_blank'>
+ <a href="https://www.linkedin.com/in/anil-rajput-6411a4233/" className='home_social-icon' target='_blank' rel='noreferrer'>
         <i class='uil uil-linkedin'></i>
 
         </a>
 
-        <a href="https://dev.to/04anilr" className='home_social-icon' target='_blank'>
+        <a href="https://dev.to/04anilr" className='home_social-icon' target='_blank' rel='noreferrer'>
             <i class='uil'><FaDev /></i>
 
         </a>
-        <a href="https://www.codingninjas.com/studio/profile/04anilr" className='home_social-icon' target='_blank'>
+        <a href="https://www.codingninjas.com/studio/profile/04anilr" className='home_social-icon' target='_blank' rel='noreferrer'>
         <i class='uil'><SiMusicbrainz /></i>
 
         </a>
-        <a href="https://github.com/04anilr" className='home_social-icon' target='_blank'>
+        <a href="https://github.com/04anilr" className='home_social-icon' target='_blank' rel='noreferrer'>
         <i class='uil uil-github-alt'></i>
 
         </a>
